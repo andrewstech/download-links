b16aee147ad62c3788d3c1f7fcda64b6550dfab5
andrewstech <45342431+andrewstech@users.noreply.github.com>
2021-04-26 19:15:52 +0100

    Update linux.sh

--- projects/alpha-video/linux.sh
+++ projects/alpha-video/linux.sh
@@ -4,4 +4,10 @@ sudo apt install unzip wget
 cd /var
 wget https://github.com/andrewstech/download-links/raw/main/files/Alpha-video-Linux.zip
 unzip Alpha-video-Linux.zip
-
+sudo chmod +x /var/alpha-video/alpha-video
+cd /etc/systemd/system/
+wget https://github.com/andrewstech/download-links/raw/main/files/alpha-video.service
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+node -v
+npm install -g localtunnel
